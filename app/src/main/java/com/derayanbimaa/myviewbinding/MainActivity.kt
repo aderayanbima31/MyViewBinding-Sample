b9d061fd726813006991b93739b2c46ef84d5835
Derayan Bima Alamsyah <derayan.bima@dana.id>
2020-03-11 10:40:54 +0700

    Tidy up code

--- app/src/main/java/com/derayanbimaa/myviewbinding/MainActivity.kt
+++ app/src/main/java/com/derayanbimaa/myviewbinding/MainActivity.kt
@@ -13,23 +13,25 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.editext.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                //no-op
+        binding.apply {
+            editext.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                    //no-op
+                }
+
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                    //no-op
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    button.isEnabled = count != 0
+                }
+
+            })
+
+            button.setOnClickListener {
+                Toast.makeText(applicationContext, binding.editext.text, Toast.LENGTH_SHORT).show()
             }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                //no-op
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                binding.button.isEnabled = count != 0
-            }
-
-        })
-
-        binding.button.setOnClickListener {
-            Toast.makeText(this, binding.editext.text, Toast.LENGTH_SHORT).show()
         }
     }
 }
